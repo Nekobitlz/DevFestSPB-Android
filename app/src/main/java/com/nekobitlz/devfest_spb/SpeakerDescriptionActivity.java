39298e3dd8cb0773430cab83d021632bcd5cffdb
Nekobitlz <jokers.bit@gmail.com>
2019-04-11 16:36:40 +0300

    Added information about speakers and activity that displays it

---
+++ app/src/main/java/com/nekobitlz/devfest_spb/SpeakerDescriptionActivity.java
@@ -0,0 +1,121 @@
+package com.nekobitlz.devfest_spb;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/*
+    Activity with full information about the selected speaker
+*/
+public class SpeakerDescriptionActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView speakerName;
+    private CircleImageView speakerImage;
+    private TextView speakerLocation;
+    private TextView speakerPosition;
+    private TextView speakerDescription;
+
+    private Button speakerLabel;
+    private TextView speakerAddress;
+    private TextView speakerTitle;
+    private TextView speakerDate;
+
+    private SpeakerInformation currentSpeaker;
+    private LectureInformation currentLecture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speaker_description);
+
+        speakerName = findViewById(R.id.speaker_name);
+        speakerImage = findViewById(R.id.speaker_image2);
+        speakerPosition = findViewById(R.id.speaker_position);
+        speakerLocation = findViewById(R.id.speaker_location);
+
+        speakerLabel = findViewById(R.id.speaker_label);
+        speakerDescription = findViewById(R.id.speaker_description);
+        speakerTitle = findViewById(R.id.speaker_title);
+        speakerAddress = findViewById(R.id.speaker_address);
+        speakerDate = findViewById(R.id.speaker_date);
+
+        //Obtains information about the selected lecture from Main Activity
+        currentLecture = (LectureInformation) getIntent().getSerializableExtra("lecture");
+        currentSpeaker = (SpeakerInformation) getIntent().getSerializableExtra("speaker");
+
+        speakerTitle.setOnClickListener(this);
+        speakerLabel.setOnClickListener(this);
+
+        setup(currentLecture);
+        setup(currentSpeaker);
+    }
+
+    /*
+        Installs information from the speaker lecture on views
+    */
+    private void setup(LectureInformation currentLecture) {
+        //Sets label background color
+        switch (currentLecture.getLabel()) {
+            case "Android" : speakerLabel.setBackgroundColor(Color.parseColor("#ff460e"));
+            break;
+            case "Frontend" : speakerLabel.setBackgroundColor(Color.parseColor("#0e6bff"));
+            break;
+            case "Common" : speakerLabel.setBackgroundColor(Color.parseColor("#5501cd"));
+            break;
+        }
+
+        speakerLabel.setText(currentLecture.getLabel());
+        speakerAddress.setText(currentLecture.getAddress());
+        speakerTitle.setText(currentLecture.getTitle());
+        speakerDate.setText(currentLecture.getDate());
+    }
+
+    /*
+        Installs information from the selected speaker on views
+    */
+    private void setup(SpeakerInformation currentSpeaker) {
+        speakerName.setText(currentSpeaker.getName());
+        speakerImage.setImageResource(
+                getResources().getIdentifier(currentSpeaker.getImage(), "drawable", getPackageName()));
+        speakerPosition.setText(currentSpeaker.getPosition());
+        speakerLocation.setText(currentSpeaker.getLocation());
+        speakerDescription.setText(currentSpeaker.getSpeakerDescription());
+    }
+
+    /*
+       Reads user clicks:
+           "speaker_title" -> Opens activity of the selected lecture
+           "label" -> Shows a toast
+   */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.speaker_title: {
+                Intent intent = new Intent(this, LectureDescriptionActivity.class);
+                intent.putExtra("lecture", currentLecture);
+                intent.putExtra("speaker", currentSpeaker);
+                startActivity(intent);
+            }
+            break;
+
+            case R.id.speaker_label: {
+                Toast.makeText(this, "HEY, DON'T CLICK PLS, IT'S JUST A TAG", Toast.LENGTH_SHORT).show();
+            }
+            break;
+        }
+    }
+
+    /*
+        If "back" button is pressed - returns to speaker activity
+    */
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+}
---
+++ app/src/main/java/com/nekobitlz/devfest_spb/SpeakerInformation.java
@@ -0,0 +1,57 @@
+package com.nekobitlz.devfest_spb;
+
+import java.io.Serializable;
+
+/*
+    Class for storing speaker data
+*/
+public class SpeakerInformation implements Serializable {
+    private String name;
+    private String image;
+    private String position;
+    private String location;
+    private String speakerDescription;
+    private String lecture;
+
+    /*
+        Speaker initialization
+    */
+    public SpeakerInformation(String name, String image, String position,
+                              String location, String speakerDescription, String lecture) {
+        this.name = name;
+        this.image = image;
+        this.position = position;
+        this.location = location;
+        this.lecture = lecture;
+
+        //In the xml file for readability, we add a line break that we need to remove
+        this.speakerDescription = speakerDescription.replaceAll("[\\s]{2,}", " ");
+    }
+
+    /*
+        GETTERS
+    */
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getSpeakerDescription() {
+        return speakerDescription;
+    }
+
+    public String getLecture() {
+        return lecture;
+    }
+}
