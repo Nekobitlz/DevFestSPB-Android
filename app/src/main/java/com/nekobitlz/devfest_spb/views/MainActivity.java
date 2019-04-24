c83a628853a189676b0df6d82f423ad73a88b870
Nekobitlz <jokers.bit@gmail.com>
2019-04-24 23:35:52 +0300

    LoadInfoTask is now a static class

--- app/src/main/java/com/nekobitlz/devfest_spb/views/MainActivity.java
+++ app/src/main/java/com/nekobitlz/devfest_spb/views/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nekobitlz.devfest_spb.views;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,36 +13,16 @@ import com.nekobitlz.devfest_spb.data.SpeakerInformation;
 import com.nekobitlz.devfest_spb.adapters.SpeakerRecyclerViewAdapter;
 import org.xmlpull.v1.XmlPullParser;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
 /*
     Main menu on which is a list of lectures
 */
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<LectureInformation> lecturesInformation;
-    private ArrayList<SpeakerInformation> speakersInformation;
-
-    private String speakerName;
-    private String date;
-    private String address;
-    private String title;
-    private String label;
-    private String lectureDescription;
-
-    private String speakerDescription;
-    private String image;
-    private String lecture;
-    private String location;
-    private String name;
-    private String position;
-
-    private RecyclerView speakersRecyclerView;
-    private SpeakerRecyclerViewAdapter adapter;
-
-    private XmlPullParser speakerParser;
-    private XmlPullParser lectureParser;
 
     private LoadInfoTask loadInfoTask;
+    private RecyclerView speakersRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,75 +32,8 @@ public class MainActivity extends AppCompatActivity {
         speakersRecyclerView = findViewById(R.id.speakers_recycler_view);
         speakersRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        lectureParser = getResources().getXml(R.xml.lectures);
-        speakerParser = getResources().getXml(R.xml.speakers);
-
-        this.lecturesInformation = new ArrayList<>();
-        this.speakersInformation = new ArrayList<>();
-
-        parseLecturesXml(lectureParser);
-        parseSpeakerXml(speakerParser);
-
-        loadInfoTask = new LoadInfoTask();
+        loadInfoTask = new LoadInfoTask(this, speakersRecyclerView);
         loadInfoTask.execute();
-    }
-
-    /*
-        Parses XML with speakers and writes all information into the list
-    */
-    private void parseSpeakerXml(XmlPullParser parser) {
-        try {
-            while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
-                if (parser.getEventType() == XmlPullParser.START_TAG && parser.getName().equals("speaker")) {
-                    //Gets xml attributes
-                    speakerDescription = parser.getAttributeValue(0);
-                    image = parser.getAttributeValue(1);
-                    lecture = parser.getAttributeValue(2);
-                    location = parser.getAttributeValue(3);
-                    name = parser.getAttributeValue(4);
-                    position = parser.getAttributeValue(5);
-
-                    //Adds speakers attributes in one big list from which we can then take items
-                    speakersInformation.add(
-                            new SpeakerInformation(name, image, position, location, speakerDescription, lecture)
-                    );
-                }
-
-                parser.next();
-            }
-        } catch (Throwable t) {
-            Toast.makeText(this,
-                    "Error loading speaker list: " + t.toString(), Toast.LENGTH_LONG).show();
-        }
-    }
-
-    /*
-        Parses XML with lectures and writes all information into the list
-    */
-    private void parseLecturesXml(XmlPullParser parser) {
-        try {
-            while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
-                if (parser.getEventType() == XmlPullParser.START_TAG && parser.getName().equals("lecture")) {
-                    //Gets xml attributes
-                    address = parser.getAttributeValue(0);
-                    date = parser.getAttributeValue(1);
-                    lectureDescription = parser.getAttributeValue(2);
-                    label = parser.getAttributeValue(3);
-                    speakerName = parser.getAttributeValue(4);
-                    title = parser.getAttributeValue(5);
-
-                    //Adds lectures attributes in one big list from which we can then take items
-                    lecturesInformation.add(
-                            new LectureInformation(speakerName, date, address, title, label, lectureDescription)
-                    );
-                }
-
-                parser.next();
-            }
-        } catch (Throwable t) {
-            Toast.makeText(this,
-                    "Error loading lecture list: " + t.toString(), Toast.LENGTH_LONG).show();
-        }
     }
 
     @Override
@@ -131,10 +45,46 @@ public class MainActivity extends AppCompatActivity {
 
     /*
         Asynchronous data loading
-
-        TODO(): Fix "This AsyncTask class should be static or leaks might occur"
     */
-    public class LoadInfoTask extends AsyncTask<Void, Void, Void> {
+    public static class LoadInfoTask extends AsyncTask<Void, Void, Void> {
+
+        private ArrayList<LectureInformation> lecturesInformation;
+        private ArrayList<SpeakerInformation> speakersInformation;
+
+        private String speakerName;
+        private String date;
+        private String address;
+        private String title;
+        private String label;
+        private String lectureDescription;
+
+        private String speakerDescription;
+        private String image;
+        private String lecture;
+        private String location;
+        private String name;
+        private String position;
+
+        private XmlPullParser speakerParser;
+        private XmlPullParser lectureParser;
+
+        private WeakReference<Context> weakContext;
+        private WeakReference<RecyclerView> weakSpeakersRecycler;
+        private SpeakerRecyclerViewAdapter adapter;
+
+        public LoadInfoTask(Context context, RecyclerView speakersRecyclerView) {
+
+            weakContext = new WeakReference<>(context);
+            weakSpeakersRecycler = new WeakReference<>(speakersRecyclerView);
+
+            if (weakContext != null) {
+                lectureParser = weakContext.get().getResources().getXml(R.xml.lectures);
+                speakerParser = weakContext.get().getResources().getXml(R.xml.speakers);
+            }
+
+            this.lecturesInformation = new ArrayList<>();
+            this.speakersInformation = new ArrayList<>();
+        }
 
         @Override
         protected Void doInBackground(Void... voids) {
@@ -146,8 +96,72 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Void aVoid) {
-            adapter = new SpeakerRecyclerViewAdapter(getBaseContext(), speakersInformation, lecturesInformation);
-            speakersRecyclerView.setAdapter(adapter);
+            if (weakContext != null) {
+                adapter = new SpeakerRecyclerViewAdapter(weakContext.get(), speakersInformation, lecturesInformation);
+            }
+
+            if (weakSpeakersRecycler != null) {
+                weakSpeakersRecycler.get().setAdapter(adapter);
+            }
+        }
+
+
+        /*
+            Parses XML with speakers and writes all information into the list
+        */
+        private void parseSpeakerXml(XmlPullParser parser) {
+            try {
+                while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
+                    if (parser.getEventType() == XmlPullParser.START_TAG && parser.getName().equals("speaker")) {
+                        //Gets xml attributes
+                        speakerDescription = parser.getAttributeValue(0);
+                        image = parser.getAttributeValue(1);
+                        lecture = parser.getAttributeValue(2);
+                        location = parser.getAttributeValue(3);
+                        name = parser.getAttributeValue(4);
+                        position = parser.getAttributeValue(5);
+
+                        //Adds speakers attributes in one big list from which we can then take items
+                        speakersInformation.add(
+                                new SpeakerInformation(name, image, position, location, speakerDescription, lecture)
+                        );
+                    }
+
+                    parser.next();
+                }
+            } catch (Throwable t) {
+                Toast.makeText(weakContext.get(),
+                        "Error loading speaker list: " + t.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        /*
+            Parses XML with lectures and writes all information into the list
+        */
+        private void parseLecturesXml(XmlPullParser parser) {
+            try {
+                while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
+                    if (parser.getEventType() == XmlPullParser.START_TAG && parser.getName().equals("lecture")) {
+                        //Gets xml attributes
+                        address = parser.getAttributeValue(0);
+                        date = parser.getAttributeValue(1);
+                        lectureDescription = parser.getAttributeValue(2);
+                        label = parser.getAttributeValue(3);
+                        speakerName = parser.getAttributeValue(4);
+                        title = parser.getAttributeValue(5);
+
+                        //Adds lectures attributes in one big list from which we can then take items
+                        lecturesInformation.add(
+                                new LectureInformation(speakerName, date, address, title, label, lectureDescription)
+                        );
+                    }
+
+                    parser.next();
+                }
+            } catch (Throwable t) {
+                Toast.makeText(weakContext.get(),
+                        "Error loading lecture list: " + t.toString(), Toast.LENGTH_LONG).show();
+            }
         }
     }
 
